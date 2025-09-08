b8ac8ca6337c61375a3a32f59d7e1d63b54350f2
Aditya Raj Srivastava <123008156+adityarajsrv@users.noreply.github.com>
2025-09-08 19:54:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of 1 Bits/number-of-1-bits.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+class Solution {
+    static int setBits(int n) {
+        int count = 0;
+        for(int i=0; i<32; i++){
+            if(((n>>i)&1) == 1){
+                count++;
+            }
+        }
+        return count;
+    }
+}
