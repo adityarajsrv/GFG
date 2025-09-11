2ca7110754c01d877c73941380810f895e46e0a5
Aditya Raj Srivastava <123008156+adityarajsrv@users.noreply.github.com>
2025-09-11 16:15:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The OR Gate/the-or-gate.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+class Solution {
+    static int orGate(int arr[], int N) {
+        for(int num:arr){
+            if(num == 1){
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
