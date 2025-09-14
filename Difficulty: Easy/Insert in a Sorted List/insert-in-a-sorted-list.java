82a3aa0c99c54c5f9a86809e63620b23084c7b9c
Aditya Raj Srivastava <123008156+adityarajsrv@users.noreply.github.com>
2025-09-14 22:34:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insert in a Sorted List/insert-in-a-sorted-list.java
@@ -0,0 +1,29 @@
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }*/
+
+// sortedInsert method should return the head of the modified linked list.
+class Solution {
+    Node sortedInsert(Node head, int key) {
+        Node newNode = new Node(key);
+        if(head == null) return newNode;
+        
+        if(head.data >= key){
+            newNode.next = head;
+            return newNode;
+        }
+        
+        Node curr = head;
+        while(curr.next != null && curr.next.data < key){
+            curr = curr.next;
+        }
+        
+        newNode.next = curr.next;
+        curr.next = newNode;
+        
+        return head;
+    }
+}
